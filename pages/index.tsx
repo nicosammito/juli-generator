937bba5efacdc2acfff7da7357a7899c820da951
nicosammito <kynxyd-bUxnek-9vohsi>
2022-06-06 16:32:45 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,8 +10,6 @@ let changeState: { "size": Function, "design": Function, "template": Function, "
 const Tool: NextPage = () => {
     return (
         <>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@900&display=swap"
                   rel="stylesheet"/>
             <NavBar/>
