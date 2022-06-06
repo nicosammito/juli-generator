1dab086333e3219e97c4bdc7e84331e108b29ddd
nicosammito <kynxyd-bUxnek-9vohsi>
2022-06-06 15:26:37 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: 'https://cdn.statically.io/gh/nicosammito/nicosammito.github.io/juli-generator/gh-pages/',
 }
 
 module.exports = nextConfig
