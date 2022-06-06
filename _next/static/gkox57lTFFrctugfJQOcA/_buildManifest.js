2da112586cdbc969959a68c2196026e53bc404da
nicosammito <60352747+nicosammito@users.noreply.github.com>
2022-06-06 13:14:26 +0000

    Deploying to gh-pages from @ nicosammito/juli-generator@07d7f85e37b52f280003432d112032ff15fcb6f1 🚀

---
+++ _next/static/gkox57lTFFrctugfJQOcA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ef6529d7-332e3011d3e065cd.js","static/chunks/768-9d5ecca05baadb54.js","static/chunks/pages/index-d75e54691dba5b86.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
